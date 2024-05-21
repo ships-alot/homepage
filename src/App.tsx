78e2742bcc544fbd09f1e1cc9c8e789f7b8ec5c8
Marcello Chiaramonte <marcello.chiaramonte@gmail.com>
2024-05-21 13:47:17 +0200

    added libraries

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import LandingPage from "./LandingPage"
+
+const App = () => {
+  return (
+    <div>
+      <LandingPage />
+    </div>
+  )
+}
+
+// const Block = () => {
+//   return (
+//     <div
+//       style={{
+//         height: "300px",
+//         backgroundColor: "red",
+//         marginBottom: "20px",
+//       }}
+//     >
+//       hello
+//     </div>
+//   )
+// }
+
+// const Partners = () => {
+//   return <div>Proudly working width: Samsung Apple Roche</div>
+// }
+
+export default App
