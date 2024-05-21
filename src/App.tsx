30981cfc7210c9152983e50e25f5ff7be5714c42
Marcello Chiaramonte <marcello.chiaramonte@gmail.com>
2024-05-21 14:19:05 +0200

    tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-import LandingPage from "./LandingPage"
-
 const App = () => {
   return (
-    <div>
-      <LandingPage />
+    <div className="w-screen">
+      <Header />
+      <Projects />
     </div>
   )
 }
+
+const Projects = () => <div>Projects</div>
+
+const Header = () => (
+  <div className="w-full">
+    <div className="flex h-16 items-center pl-4">
+      <div>Name Lastname</div>
+      <div>CXO</div>
+    </div>
+    <div className="h-0.5 bg-sky-600"></div>
+  </div>
+)
 
 // const Block = () => {
 //   return (
